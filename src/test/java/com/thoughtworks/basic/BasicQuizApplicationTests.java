c29fbbd4c282fc89a940b182d3c3bdb830f5cf0e
zuowen.Wang <798389920@qq.com>
2020-09-15 11:05:39 +0800

    init

---
+++ src/test/java/com/thoughtworks/basic/BasicQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
